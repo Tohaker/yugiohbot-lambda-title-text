fbaab952f82b6707d3a2388b0dcf0c86a29eb8c4
milesbardon <milesbardon@gmail.com>
2019-10-16 21:27:07 +0100

    Integration test is now a script

---
+++ integration.sh
@@ -0,0 +1,9 @@
+RESULT=$(docker run -e NLTK_DATA=/var/task/nltk_data -e AWS_LAMBDA_FUNCTION_MEMORY_SIZE=128 \
+ --rm -v "$PWD"/package:/var/task lambci/lambda:python3.7 lambda_function.lambda_handler)
+
+if grep -q title "$RESULT"; then
+  exit 0
+else
+  exit 1
+fi
+
